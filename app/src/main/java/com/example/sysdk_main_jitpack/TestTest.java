cb55310cbca2031ab56f8a10f5603b117444f8e8
moke <1805593886@qq.com>
2021-10-20 18:13:45 +0800

    2+

---
+++ app/src/main/java/com/example/sysdk_main_jitpack/TestTest.java
@@ -0,0 +1,5 @@
+package com.example.sysdk_main_jitpack;
+
+public class TestTest {
+    public static void testtest(){}
+}
---
+++ sysdk/src/main/java/com/example/sysdk/TestTest.java
@@ -0,0 +1,5 @@
+package com.example.sysdk;
+
+public class TestTest {
+    public static void testtest(){}
+}
